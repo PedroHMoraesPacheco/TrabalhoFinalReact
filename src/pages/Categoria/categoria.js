420731ed709af6a823f17939832a04a821c7104f
BarbaraEspenchidt <barbaraespenchidt@hotmail.com>
2022-06-18 21:20:46 -0300

    Categoria
    Página para CRUD de Categoria

---
+++ src/pages/Categoria/categoria.js
@@ -0,0 +1,7 @@
+
+function AdicionarCategoria(){
+    return <h1>Adicionar Categoria</h1>
+
+}
+
+export default AdicionarCategoria
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from '../pages/Login'
 import { Home } from '../pages/Home'
 
+
 export function AppRoutes(){
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login />}/>
                 <Route path="/home" element={<Home />}/>
+               
             </Routes>
         </BrowserRouter>
     )
