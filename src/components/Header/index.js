e0981a202c18d2c87cca2d35168cc29d0c1dc5a9
Luiz Felipe Canário Costa <luizfelipecanariocosta@gmail.com>
2022-06-19 20:37:36 -0300

    New

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,16 @@
 import React from 'react'
 
-import logo from "../../public/header.jpg";
+// import logo from "../../public/header.jpg";
 import { Container } from "./styles";
 
 export const Header = () => {
   return (
         <Container>
         <div className="header">
-            <img src={logo} alt="header.jpg" className="logo"/>
+            {/* <img src={logo} alt="header.jpg" className="logo"/> */}
             <button className="salmon-btn">+ INFO</button>
         </div>
         </Container>
   )
 }
 
-export default Header;
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,16 +5,21 @@ import { Login } from '../pages/Login'
 import { Home } from '../pages/Home'
 import { Cadastro } from "../pages/Cadastro";
 import { Categoria } from '../pages/Categoria'
+import Footer from "../components/Footer/footer";
+import { Header } from "../components/Header";
+
 
 export function AppRoutes() {
     return (
         <BrowserRouter>
+          <Header/>
             <Routes>
              <Route path="/" element={<Login />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/cadastro" element={<Cadastro />} />
                 <Route path="/categoria" element={<Categoria/>}/>
             </Routes>
+            <Footer />
         </BrowserRouter>
     )
 }
