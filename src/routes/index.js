bde09e3d05352f51be104442d3570f62a39fb2f2
BarbaraEspenchidt <barbaraespenchidt@hotmail.com>
2022-06-19 19:51:19 -0300

    Footer e Categoria

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,27 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Login } from '../pages/Login'
 import { Home } from '../pages/Home'
-<<<<<<< Updated upstream
 import { Cadastro } from "../pages/Cadastro";
-=======
 import { Categoria } from '../pages/Categoria'
->>>>>>> Stashed changes
-
 
 export function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-<<<<<<< Updated upstream
-                <Route path="/" element={<Login />} />
+             <Route path="/" element={<Login />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/cadastro" element={<Cadastro />} />
-=======
-                <Route path="/" element={<Login />}/>
-                <Route path="/home" element={<Home />}/>
                 <Route path="/categoria" element={<Categoria/>}/>
-               
->>>>>>> Stashed changes
             </Routes>
         </BrowserRouter>
     )
