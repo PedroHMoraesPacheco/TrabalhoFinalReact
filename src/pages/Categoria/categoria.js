2c8cd511967dc854170c83a7f94de6beb6e3b56d
BarbaraEspenchidt <barbaraespenchidt@hotmail.com>
2022-06-18 22:53:59 -0300

    Cadastro de Categoria
    Página Cadastro de Categoria, formulário específico de categoria e form com input geral para reutilização(parcial)

--- src/pages/Categoria/categoria.js
+++ src/pages/Categoria/categoria.js
@@ -1,7 +1,14 @@
+import CategoriaForm from '../../components/CategoriaForms/categoriaForm'
+import styles from './categoriaStyle.css'
 
 function AdicionarCategoria(){
-    return <h1>Adicionar Categoria</h1>
-
+    return (
+        <div className= {styles.novacategoria_container}>
+        <h1>Adicionar Categoria</h1>
+        <p> Cadrastre a Categoria para depois adicionar produtos</p>
+        <p>CategoriaForm</p>
+    </div>
+    )
+   
 }
-
 export default AdicionarCategoria
