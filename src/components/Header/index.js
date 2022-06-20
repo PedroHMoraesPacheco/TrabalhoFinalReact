366a16225c3ac6c08b847ac23c5bf098fd798674
João Marcos Bernardes <102327585+JM-Bernardes@users.noreply.github.com>
2022-06-20 18:35:49 -0300

    .

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,1 @@
-import React from 'react'
-
-// import logo from "../../public/header.jpg";
-import { Container } from "./styles";
-
-export const Header = () => {
-  return (
-        <Container>
-        <div className="header">
-            {/* <img src={logo} alt="header.jpg" className="logo"/> */}
-            <button className="salmon-btn">+ INFO</button>
-        </div>
-        </Container>
-  )
-}
 
