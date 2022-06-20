1f7575c29be094587aba1675b7a8ce05736c162f
BarbaraEspenchidt <barbaraespenchidt@hotmail.com>
2022-06-19 23:23:46 -0300

    Form e Input

--- src/pages/Categoria/index.js
+++ src/pages/Categoria/index.js
@@ -1,5 +1,5 @@
-import Footer from "../../components/Footer/footer"
-// import CategoriaForm from '../../components/CategoriaForms'
+
+import CategoriaForm from '../../components/CategoriaForms/categoriaForm'
 import './categoriaStyle.css'
 
 
@@ -9,11 +9,9 @@ export const Categoria= () => {
         <div className="novacategoria_container">
         <h1>Categorias</h1>
         <p>Cadastre a Categoria</p>
-        <p>formulário</p>
+        <CategoriaForm/>
         </div>
- <div>
-<Footer/>
- </div>    
+   
         </>
 
     )
