8782d57423267db571e9eed8c671b4c293f016cc
João Marcos Bernardes <102327585+JM-Bernardes@users.noreply.github.com>
2022-06-20 18:39:41 -0300

    correcao

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,16 @@
+import React from 'react'
+
+// import logo from "../../public/header.jpg";
+import { Container } from "./styles";
+
+export const Header = () => {
+  return (
+        <Container>
+        <div className="header">
+            {/* <img src={logo} alt="header.jpg" className="logo"/> */}
+            <button className="salmon-btn">+ INFO</button>
+        </div>
+        </Container>
+  )
+}
 
