ff7b3345116bc0b6a3b63d34a3a452c37d55961e
PedroHMP <102328509+PedroHMoraesPacheco@users.noreply.github.com>
2022-06-20 14:32:20 -0300

    produto

--- src/pages/Produtos/index.js
+++ src/pages/Produtos/index.js
@@ -13,18 +13,36 @@ export function Produtos() {
   const [preco, setPreco] = useState('12.0');
   const [quantidade_estoque, setQuantidade_estoque] = useState('10');
   const [categoria, setCategoria] = useState('Camisas');
+  const [file, setFile] = useState();
 
-  function handleCadastro() {
-    produtoService.Create(nome, descricao, preco, quantidade_estoque,categoria).then(res => {
-      console.log(res);
-      toast.success('produto criado')
-      navigate("/home")
+
+  //function handleCadastro() {
+
+  //   produtoService.Create(nome, descricao, preco, quantidade_estoque,categoria,file).then(res => {
+  //     uploadImages().then(event =>{
+  //       this.setState({
+  //         selectedFile: event.target.files[0]
+  //         })
+  //       })
+  //     console.log(res);
+  //     toast.success('produto criado')
+  //     navigate("/home")
+  //   }).catch(err => {
+  //     toast.error('ERRO')
+  //     console.log(err);
+  //   })
+  // }
+
+  function handleCadastro(){
+    produtoService.CreateNoImg(nome, descricao, preco, quantidade_estoque,categoria).then(res => {
+           console.log(res);
+           toast.success('produto criado')
+           navigate("/home")
     }).catch(err => {
-      toast.error('ERRO')
-      console.log(err);
-    })
+           toast.error('ERRO')
+           console.log(err);
+        })
   }
-
   return (
     <div style={{ display: 'flex', height: '100vh', backgroundColor: '#FFF', alignItems: 'center', justifyContent: 'center' }}>
       <div style={{ display: 'flex', backgroundColor: '#FF4040', width: 500, padding: 50, flexDirection: 'column', justifyContent: 'center' }}>
@@ -34,11 +52,11 @@ export function Produtos() {
         <input style={{ marginBottom: 20, padding: 15 }} placeholder='Preço' required type={'preco'} value={preco} onChange={e => setPreco(e.target.value)} />
         <input style={{ marginBottom: 20, padding: 15 }} placeholder='Estoque' required type={'quantidade_estoque'} value={quantidade_estoque} onChange={e => setQuantidade_estoque(e.target.value)} />
         <input style={{ marginBottom: 20, padding: 15 }} placeholder='Categoria' required type={'categoria'} value={categoria} onChange={e => setCategoria(e.target.value)} />
+        {/* <input style={{ marginBottom: 20, padding: 15 }} placeholder='File' required type={"file"} value={file} onChange={e => setFile(e.target.value)} /> */}
         <button style={{ marginBottom: 20, padding: 15 }} onClick={handleCadastro}>
           Cadastrar
         </button>
       </div>
-      <UploadFiles />
     </div>
   )
 }
--- src/services/products.js
+++ src/services/products.js
@@ -1,7 +1,7 @@
 import { api } from './http';
 
 const Create = (nome, descricao, preco, quantidade_estoque)=>{
-    return api.post(`produto`,{
+    return api.post(`produto/imagem`,{
         nome:nome,
         descricao:descricao,
         preco:preco,
@@ -9,6 +9,16 @@ const Create = (nome, descricao, preco, quantidade_estoque)=>{
     })
 }
 
+const CreateNoImg=(nome, descricao, preco, quantidade_estoque, categoria)=>{
+    return api.post(`produto`),{
+        nome:nome,
+        descricao:descricao,
+        preco:preco,
+        quantidade_estoque:quantidade_estoque,
+        categoria:categoria
+    }
+}
+
 export default {
-    Create
+    Create, CreateNoImg
 }
