d7c44af36b8143b079265024d1c088a14ad1bbb6
João Marcos Bernardes <102327585+JM-Bernardes@users.noreply.github.com>
2022-06-21 14:45:02 -0300

    atualizacao login header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
-//import logo from "../../assets/logo2.png";
+import Button from '../Button';
 import './styles.css';
 
 export function Header() {
   return (
         <div className="header">
-           <h1>ECOMMERCH</h1>
+           <h1><Button to={'/'} text='ECOMMERCH' /></h1>
+           <h6><Button to ={'/login'} text='Login' /></h6>
         </div>
   )
 }
