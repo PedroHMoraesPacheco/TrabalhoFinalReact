8df6db46955d57f2e54b2ca68cdb6ba230aaf4b5
Luiz Felipe Canário Costa <luizfelipecanariocosta@gmail.com>
2022-06-21 15:54:20 -0300

    new logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Button from '../Button';
 import './styles.css';
+import img from './Mplace.png'
 
 export function Header() {
   return (
+    <>
+        
         <div className="header">
+          <img src={img} alt='market'/>
            <h2><Button to={'/'} text='Home' /></h2>
            <h2><Button to={'/categoria'} text='Categorias' /></h2>
            <h3><Button to ={'/login'} text='Login' /></h3>
         </div>
+   </>     
   )
 }
