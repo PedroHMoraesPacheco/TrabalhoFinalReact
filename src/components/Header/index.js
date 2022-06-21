c51f19b6b659a6a3fa1a3d5d67d91a05dbf3c783
PedroHMP <102328509+PedroHMoraesPacheco@users.noreply.github.com>
2022-06-21 18:00:26 -0300

    Revert "."
    This reverts commit 510c0ace327368c607858bbe17364e68a4b79e3a.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,9 +9,9 @@ export function Header() {
         
         <div className="header">
           <img src={img} alt='market'/>
-          <h3><Button to ={'/login'} text='Login' /></h3>
-           <h2><Button to={'/categoria'} text='Categorias' /></h2>
            <h2><Button to={'/'} text='Home' /></h2>
+           <h2><Button to={'/categoria'} text='Categorias' /></h2>
+           <h3><Button to ={'/login'} text='Login' /></h3>
         </div>
    </>     
   )
