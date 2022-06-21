12b7e8cc6e97c9660198d99f28b7a738b8a024d8
Luiz Felipe Canário Costa <luizfelipecanariocosta@gmail.com>
2022-06-21 15:12:29 -0300

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,9 @@ import './styles.css';
 export function Header() {
   return (
         <div className="header">
-           <h1><Button to={'/'} text='ECOMMERCH' /></h1>
-           <h6><Button to ={'/login'} text='Login' /></h6>
+           <h2><Button to={'/'} text='Home' /></h2>
+           <h2><Button to={'/categoria'} text='Categorias' /></h2>
+           <h3><Button to ={'/login'} text='Login' /></h3>
         </div>
   )
 }
